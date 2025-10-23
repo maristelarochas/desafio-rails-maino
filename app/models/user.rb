72d6405b7398c66c2e5908b00438dd14bd3faab1
Maristela Rocha <maristela.rocha.silva@gmail.com>
2025-10-23 16:48:16 -0300

    feat: Environment Setup

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
+  has_many :movies, dependent: :destroy
+  has_many :comments, dependent: :destroy
+end
